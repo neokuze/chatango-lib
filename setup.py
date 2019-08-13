e9a5f8429826448884d46cc88b1ccafc26f55e70
TheClonerx <theclonerx@outlook.com>
2019-08-13 19:29:19 -0400

    Base skeleton

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+requirements = []
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+readme = ""
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    author="Yado",
+    url="https://github.com/neokuze/chatango-lib",
+    packages=["chatango"],
+    license="MIT",
+    description="A Python library for connecting to Chatango",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    python_requires=">=3.6"
+)
