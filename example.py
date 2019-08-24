6eef14ea886269e048dc216e3902304c4c525719
Yado <29682872+neokuze@users.noreply.github.com>
2019-08-24 18:11:27 +0000

    Just a example bot
    for testing and others.

--- example.py
+++ example.py
@@ -2,20 +2,44 @@ import chatango
 import asyncio
 import typing
 
-
+owners = ["neokuze"]
 class MyBot(chatango.Client):
     async def on_init(self):
         print("Bot initialized")
-        await self.join("exampleroom")
+        self.suffix = "~"
+        await self.join("examplegroup")
 
     async def on_connect(self, room: typing.Union[chatango.Room, chatango.PM]):
         print("Connected to", room)
 
-
+    async def on_message(self, message):
+        if len(message.body.split(" ",1)) > 1:
+            cmd = message.body.split(" ",1)[0].lower()
+            args = message.body.split(" ",1)[1].strip().split(" ")
+        else:
+            cmd, args = message.body.split(" ",1)[0], []
+        cmd = cmd[:-1]
+        issuffix = message.body[len(cmd):len(cmd)+len(self.suffix)] == self.suffix
+        print(f"[{message.room.name}]-[{issuffix}]-[{message.author}]: {message.body}")
+        if issuffix:
+            if cmd == "eval" and message.user.lower() in owners and args:
+                try:
+                    if args[0] == "await":
+                        args = args[1:]
+                        ret = await eval(" ".join(args))
+                    else:
+                        ret = eval(" ".join(args))
+                    if ret == None:
+                        await message.room.send_message("Done.")
+                    else: await message.room.send_message(str(ret))
+                except Exception as e:
+                    print(str(e))
+            elif cmd == "hello": # using version
+                await message.room.send_message(f"Hello @{message.user}\rI\'m running chatango-lib {chatango.ver}'")
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
     bot = MyBot()
-    bot.default_user("ExampleBot")
+    bot.default_user("Yadrier")
     loop.run_until_complete(bot.start())
     loop.run_forever()
