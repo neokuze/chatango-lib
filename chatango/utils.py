dee1b877b1b723ea272a40e96e87a3a60604b0af
Yado <imneokuze@gmail.com>
2019-09-12 12:45:43 -0400

    added a parse for flags

--- chatango/utils.py
+++ chatango/utils.py
@@ -11,3 +11,15 @@ def gen_uid() -> str:
     Generate an uid
     """
     return str(random.randrange(10 ** 15, 10 ** 16))
+
+def parse_flags(FlagInterator, flags):
+    _flags = {}
+    FLAGS = [flag for flag in dir(FlagInterator) if flag[:2] != "__"]
+    for flag in FLAGS:
+        if hasattr(FlagInterator(flags), f"{flag}"):
+            active_flag = getattr(FlagInterator(flags), f"{flag}")
+            if active_flag in FlagInterator(flags):
+                _flags[f"{flag}"] = True
+            else:
+                _flags[f"{flag}"] = False
+    return _flags
