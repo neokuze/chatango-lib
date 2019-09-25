2bf3c66b9c0ca3a81f49fc22913a078847196abd
Yado <imneokuze@gmail.com>
2019-09-25 12:17:27 -0400

    Update pm.py

--- chatango/pm.py
+++ chatango/pm.py
@@ -11,27 +11,20 @@ class PM(Connection):
     """
     Represents a PM Connection
     """
-
-    # async def _get_auth(self, user_name: str, password: str) -> typing.Optional[str]:
-    #     session: aiohttp.ClientSession
-    #     session = self.client.aiohttp_session
-    #     data = urllib.parse.urlencode({
-    #         "user_id": user_name,
-    #         "password": password,
-    #         "storecookie": "on",
-    #         "checkerrors": "yes"
-    #     }).encode()
-    #     async with session.post("http://chatango.com/login", data=data) as response:
-    #         print(response.cookies)
-
+    def __init__(self, client):
+        super().__init__(client)
+        self.server = "i0.chatango.com"
+        self._uid = gen_uid()
+        
     async def _connect(self, user_name: str, password: str):
-        # ch.py uses it but i guess it isn't necesary?
-        # auid = self._get_auth(user_name, password)
-        self._connection = await self.client.aiohttp_session.ws_connect(
-            "ws://i0.chatango.com:8080/",
-            origin="http://st.chatango.com"
-        )
-        await self._send_command("blogin", user_name, password)
+        token = await get_token(user_name, password)
+        if not token:
+            await self.client._call_event("login_fail", str(user_name))
+        else:
+            self._connection = await self.client.aiohttp_session.ws_connect(
+                f"ws://{self.server}:8080/",
+                origin="http://st.chatango.com")
+            await self._send_command("tlogin", str(token), "2", self._uid)
 
     def __repr__(self):
         return "<PM>"
