cab31470e4151fb8785b197d40d1b94d5882b0d2
Yado <imneokuze@gmail.com>
2020-02-14 23:31:12 -0400

    clean PM
    deleting chatango message catcher server from the pm.py
    another changes comming.

--- chatango/pm.py
+++ chatango/pm.py
@@ -1,11 +1,6 @@
 """
 Module for pm related stuff
 """
-from .utils import get_token, gen_uid
-from .connection import Connection
-import urllib.parse
-import aiohttp
-import typing
 import asyncio
 
 
@@ -13,20 +8,9 @@ class PM(Connection):
     """
     Represents a PM Connection
     """
-    def __init__(self, client):
+    def __init__(self, client): #clean
         super().__init__(client)
-        self.server = "i0.chatango.com"
-        self._uid = gen_uid()
-        
-    async def _connect(self, user_name: str, password: str):
-        token = await get_token(user_name, password)
-        if not token:
-            await self.client._call_event("login_fail", str(user_name))
-        else:
-            self._connection = await self.client.aiohttp_session.ws_connect(
-                f"ws://{self.server}:8080/",
-                origin="http://st.chatango.com")
-            await self._send_command("tlogin", str(token), "2", self._uid)
-
+        pass
+    
     def __repr__(self):
         return "<PM>"
