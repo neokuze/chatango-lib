419ebaae57cf92e9125cbbdbd6b7ec17f2d5ca79
Yado <imneokuze@gmail.com>
2020-04-22 01:38:26 -0400

    Update example.py

--- example.py
+++ example.py
@@ -23,18 +23,19 @@ class MyBot(chatango.Client):
             room.setFont("fontface", 1)
         else:
             await room.user.get_profile()
-        
+            await room.enable_bg()
+            
     async def on_message(self, message):
         print(time.strftime("%b/%d-%H:%M:%S", time.localtime(message.time)),
               message.room.name, message.user.showname, ascii(message.body)[1:-1])
         if message.body.startswith("!a"):
-            await message._room.send_message(f"Hello {message.user.showname}")
+            await message.channel.send(f"Hello {message.user.showname}")
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
     bot = MyBot()
-    bot.default_user("examplebot", pm=False)
+    bot.default_user("examplebot", pm=False) #True if passwd was input.
     loop.run_until_complete(bot.start())
     loop.run_forever()
