a624fa161d5f7c6cf6d0bc73954b9556953627f6
LmaoLover <hello@lmao.love>
2023-05-03 13:13:29 -0600

    Connection: format with black and clean imports

--- chatango/connection.py
+++ chatango/connection.py
@@ -1,11 +1,11 @@
-from .exceptions import AlreadyConnectedError, NotConnectedError
-
-import typing
 import asyncio
 import aiohttp
 import sys
 import traceback
-import socket
+from typing import Optional
+
+from .exceptions import AlreadyConnectedError
+
 
 class Connection:
     def __init__(self, client):
@@ -16,7 +16,11 @@ class Connection:
         self._ping_task = None
         self._first_command = True
 
-    async def connect(self, user_name: typing.Optional[str] = None, password: typing.Optional[str] = None):
+    async def connect(
+        self,
+        user_name: Optional[str] = None,
+        password: Optional[str] = None,
+    ):
         if self.connected:
             raise AlreadyConnectedError(getattr(self, "name", None), self)
         self._first_command = True
@@ -45,7 +49,8 @@ class Connection:
             # ping is an empty message
             await self._send_command("\r\n", terminator="\x00")
             await self.client._call_event("ping", self)
-            if not self.connected: break        
+            if not self.connected:
+                break
 
     async def _do_recv(self):
         while True:
@@ -62,16 +67,18 @@ class Connection:
                 if hasattr(self, f"_rcmd_{cmd}"):
                     try:
                         await getattr(self, f"_rcmd_{cmd}")(args)
-                    except (asyncio.exceptions.CancelledError):
+                    except asyncio.exceptions.CancelledError:
                         break
                     except:
                         if self.client.debug:
-                            print("Error while handling command",
-                              cmd, file=sys.stderr)
+                            print("Error while handling command", cmd, file=sys.stderr)
                             traceback.print_exc(file=sys.stderr)
                 elif self.client.debug:
-                    print(self, "Unhandled received command", cmd, args, file=sys.stderr)
-                if not self.connected: break
+                    print(
+                        self, "Unhandled received command", cmd, args, file=sys.stderr
+                    )
+                if not self.connected:
+                    break
             except aiohttp.WSServerDisconnectedError:
                 print("Lost connection to server")
                 await self.cancel()
@@ -81,9 +88,8 @@ class Connection:
                 except Exception as e:
                     print(f"Failed to reconnect: {e}")
 
-              
-        
-class Socket: #resolver for socket client
+
+class Socket:  # resolver for socket client
     def __init__(self, client):
         self.client = client
         self._first = True
@@ -93,14 +99,19 @@ class Socket: #resolver for socket client
         self._recv_task = None
         self._ping_task = None
 
-    async def connect(self, user_name: typing.Optional[str] = None, password: typing.Optional[str] = None):
+    async def connect(
+        self,
+        user_name: Optional[str] = None,
+        password: Optional[str] = None,
+    ):
         """
         user_name, password. For the socket client
         """
         if self.connected:
             raise AlreadyConnectedError(getattr(self, "name", None), self)
         self._recv, self._connection = await asyncio.open_connection(
-            f"{self.server}", self.port)
+            f"{self.server}", self.port
+        )
         self._recv_task = asyncio.create_task(self._do_recv())
         self._ping_task = asyncio.create_task(self._do_ping())
         await self._login(user_name, password)
@@ -115,7 +126,7 @@ class Socket: #resolver for socket client
         self._ping_task.cancel()
         await self._connection.close()
 
-    async def _send_command(self, *args, terminator="\r\n\0"): 
+    async def _send_command(self, *args, terminator="\r\n\0"):
         if self._first_command:
             terminator = "\x00"
             self._first_command = False
@@ -131,25 +142,28 @@ class Socket: #resolver for socket client
             # ping is an empty message
             await self._send_command("\r\n", terminator="\x00")
             await self.client._call_event("ping", self)
-            if not self.connected: break  
+            if not self.connected:
+                break
 
     async def _do_recv(self):
         while True:
-            rcv = await self._recv.read(2048) #TODO if the rcv is higher than bytes, may is cutted
+            # TODO if the rcv is higher than bytes, may is cutted
+            rcv = await self._recv.read(2048)
             await asyncio.sleep(0.0001)
             if rcv:  # si recibe datos.
                 data = rcv.decode()
-                if data == "\r\n\x00": #pong
+                if data == "\r\n\x00":  # pong
                     await self._do_process("")
                 else:
-                    recv = data.split("\r\n\x00") #event
+                    recv = data.split("\r\n\x00")  # event
                     for r in recv:
                         if r != "":
                             await self._do_process(r)
             else:
                 self._recv.close()
                 print(f"Disconnected from {self}")
-            if not self.connected: break  
+            if not self.connected:
+                break
         raise ConnectionAbortedError
 
     async def _do_process(self, recv):
@@ -167,8 +181,7 @@ class Socket: #resolver for socket client
                 await getattr(self, f"_rcmd_{cmd}")(args)
             except:
                 if __debug__:
-                    print("Error while handling command",
-                          cmd, file=sys.stderr)
+                    print("Error while handling command", cmd, file=sys.stderr)
                     traceback.print_exc(file=sys.stderr)
         elif __debug__:
             print("Unhandled received command", cmd, file=sys.stderr)
