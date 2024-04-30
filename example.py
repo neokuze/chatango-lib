58f7b64d124e7d42345b9fbea538b6801fd92bea
Yado <imyadiel@gmail.com>
2024-04-30 09:07:05 -0400

    fix and proper init

--- example.py
+++ example.py
@@ -8,31 +8,32 @@ import typing
 class config:
     user_name = ""
     passwd = ""
-    rooms = ["asynclibraryinpython"]
+    rooms = ["examplegroup"]
     pm = False
 
-class MyBot(chatango.Client):
+class Bot(chatango.Client):
     async def on_connect(self, room: typing.Union[chatango.Room, chatango.PM]):
         print("[info] Connected to {}".format(repr(room)))
-       
     async def on_disconnect(self, room):
         print("[info] Disconnected from {}".format(repr(room)))
-   async def on_message(self, message):
+    async def on_message(self, message):
         print(time.strftime("%b/%d-%H:%M:%S", time.localtime(message.time)),
               message.room.name, message.user.showname, ascii(message.body)[1:-1])
+        if message.body.split()[0] in ["!a"]:
+            await message.room.send_message("test") 
        
 """
 Specials thanks to LmaoLover, TheClonerx
 """
+bot = Bot(config.user_name, config.passwd, config.rooms , pm=config.pm)
+async def main ():
+    await bot.run(forever=True)
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    bot = Bot(config.user_name, config.passwd, config.rooms , pm=config.pm)
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(bot.run(forever=True))
+        loop.run_until_complete(main())
         loop.run_forever()
     except KeyboardInterrupt:
         print("[KeyboardInterrupt] Killed bot.")
